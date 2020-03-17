2289152b0f1cf4ac2ab319d0e32981a78c31756b
thegetoutgirl <amyahaha@gmail.com>
2020-03-17 14:22:52 -0400

    updated create collaborator model

--- models/collab.js
+++ models/collab.js
@@ -1,7 +1,36 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+  var Collab = sequelize.define("Post", {
+    fName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // skills {
+    //   dropdown menu
+    // },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zip: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+  return Collab;
   });
-  return Example;
-};
+}
