91ff4d6369a409ef257a569bb7f9f3dbeb686a7d
thegetoutgirl <amyahaha@gmail.com>
2020-03-17 14:01:50 -0400

    folders and starter code

---
+++ models/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS collabhub;
+CREATE DATABASE collabhub;
+
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  fName VARCHAR(200) NOT NULL,
+  lName VARCHAR(200) NOT NULL,
+  emailAddress VARCHAR(200) NOT NULL,
+--   skills dropdown
+  city VARCHAR(30),
+  state VARCHAR(2),
+  zip VARCHAR(5),
+  bio TEXT,
+  PRIMARY KEY (id)
+);
+
+-- DROP DATABASE IF EXISTS skills;
+-- CREATE DATABASE skills;
+
+-- CREATE TABLE skillset (
+--   id INT NOT NULL AUTO_INCREMENT,
+--   uxui VARCHAR(20),
+--   databaseEngineer VARCHAR(20),
+--   fullStack VARCHAR(20),
+--   serverSide VARCHAR(20),
+--   iOSapp VARCHAR(20),
+--   androidApp VARCHAR(20),
+--   linux VARCHAR(20),
+--   java VARCHAR(20),
+--   aws VARCHAR(20),
+--   perl VARCHAR(20),
+--   PRIMARY KEY (id)
+-- );
